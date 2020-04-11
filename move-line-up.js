a848b9bb5a9e9aac8d1efbffa9d81590f9737ef4
justan0theruser <39777723+justan0theruser@users.noreply.github.com>
2020-04-11 13:22:33 -0500

    Create move-line-up.js

---
+++ move-line-up.js
@@ -0,0 +1,13 @@
+
+// source: ???
+
+range = editor.getSelectedLineRange();
+line = editor.getTextInRange(range[0], range[1]);
+
+if (range[0] > 0) {
+	editor.replaceTextInRange(range[0] - 1, range[1], "");
+	
+	range = editor.getSelectedLineRange();
+	editor.replaceTextInRange(range[0], 0, line + "\n");
+	editor.setSelectedRange(range[0], range[0]);
+}
